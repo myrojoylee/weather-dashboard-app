2611a2ee95c03f1e945c6e5a914837e31f4aad3b
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-04-11 09:14:11 -0400

    code can display current city weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,25 @@ let currentCity = document.querySelector(".current-city");
 let currentTemp = document.querySelector(".current-temp");
 let currentWindSpeed = document.querySelector(".current-wind-speed");
 let currentHumidity = document.querySelector(".current-humidity");
+let todaysDate = document.querySelector(".current-date");
 
+// date
+
+// =================================================
+// Time and date variables
+// =================================================
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+// let twoDigitYear = Number(currentYear.toString().slice(2, 4));
+let currentMonth = currentDate.getMonth() + 1;
+let currentDayOfMonth = currentDate.getDate();
+let currentHour = currentDate.getHours();
+let currentMinutes = currentDate.getMinutes();
+
+let weatherEmoji = [{ sun: "☀", cloud: "☁" }];
+// =================================================
+//          ------------ Code -----------
+// =================================================
 search.addEventListener("click", function () {
   let queryURL =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
@@ -27,7 +45,12 @@ function cityData(weather) {
   let windSpeedMetric = weather.wind.speed;
   let windSpeedImperial = (windSpeedMetric * 2.237).toFixed(1);
   currentCity.textContent = userCity.value;
+  todaysDate.textContent = ` ${currentMonth}/${currentDayOfMonth}/${currentYear}`;
   currentTemp.textContent = `${degF}`;
   currentWindSpeed.textContent = `${windSpeedImperial}`;
   currentHumidity.textContent = `${weather.main.humidity}`;
+}
+
+function quickFetch() {
+  //quick search from recent search history
 }
